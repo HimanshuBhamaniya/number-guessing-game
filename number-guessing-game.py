9fa3de5902b6dd9c4ae9a7f78dcfd82bac7f179b
swatiateng <swatiateng@gmail.com>
2026-08-07 23:15:09 +0530

    fixed Value Error,Name Error

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -13,22 +13,32 @@ level = {
     2 : 5,
     3 : 3
 }
-dificulty = int(input('Enter your choice > '))
-if dificulty not in level.keys():
-    print('Choose from 1 (Easy), 2 (Medium) or 3 (Hard)')
-else:
-    print(f"Great! You have selected the {dificulty} difficulty level. Let's start the game!")
-    count = 0
-    for guesses_taken in range(level[dificulty]):
-        guess = int(input('Enter your guess > '))
-        count +=1
-        if guess < secret_number:
-            print('Incorrect! Your guess is too low.')
-        elif guess > secret_number:
-            print('Incorrect! Your guess is too high.')
+
+try:
+    dificulty = int(input('Enter your choice > '))
+
+
+    if dificulty not in level.keys():
+        print('Choose from 1 (Easy), 2 (Medium) or 3 (Hard)')
+    else:
+        print(f"Great! You have selected the {dificulty} difficulty level. Let's start the game!")
+        count = 0
+        for guesses_taken in range(level[dificulty]):
+            
+            guess = int(input('Enter your guess > '))
+            count +=1
+            if guess < secret_number:
+                print('Incorrect! Your guess is too low.')
+            elif guess > secret_number:
+                print('Incorrect! Your guess is too high.')
+        
+            if guess == secret_number:
+                print('Good job! You got it in '+str(count)+' guesses.')
+                break
+
+            
         else:
-            break
-if guess == secret_number:
-    print('Good job! You got it in '+str(count)+' guesses.')
-else:
-    print('Nope. The number was '+str(secret_number)+'.')
+            print('Nope. The number was '+str(secret_number)+'.')
+
+except ValueError:
+    print('Please enter a valid number.')
